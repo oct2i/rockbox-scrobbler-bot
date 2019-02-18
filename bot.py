260631d33c848086092c9eb9b11f09155bff041e
oct2i <oct2i@yandex.ru>
2019-02-18 22:10:20 +0700

    Init branch 'develop'

--- bot.py
+++ bot.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """This file contains that represents a Telegram Bot."""
+
+def start():
+    pass
+
+def error():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
