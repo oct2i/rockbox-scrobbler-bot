40710296da466dc70621790f5176edf203aa43ba
oct2i <oct2i@yandex.ru>
2019-02-18 20:57:26 +0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This file contains that represents a Telegram Bot."""
---
+++ config.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+"""This file contains configuration the Telegram Bot."""
